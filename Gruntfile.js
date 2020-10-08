3fb2ea12446aa58d66848fc22c92d0dcc17248bb
FSSlc <fsslc235@gmail.com>
2020-10-08 18:46:47 +0800

    update dev pkg's version; fix yarn run test error; replace Buffer to Buffer.from
    Signed-off-by: FSSlc <fsslc235@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function (grunt) {
                          * It's either this or bundle a copy of the library with those few bytes of changes.
                          */
                         src = src.toString("utf8").replace(/(function isIdentifierStart\(ch\) {\s+return)/m, '$1 (ch == 0x40) || ');
-                        next(err, new Buffer(src, "utf8"));
+                        next(err, new Buffer.from(src, "utf8"));
                     }
                 }
             }
